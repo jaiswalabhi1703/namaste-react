9f51a4ea03da45e03b5ba0f7cbe42a40badc7129
jaiswalabhi1703 <96300093+jaiswalabhi1703@users.noreply.github.com>
2024-06-27 19:27:16 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,8 @@
+const div=React.createElement("div",{},React.createElement("div",{},[
+    React.createElement("h1",{},"Sytem hai"),
+    React.createElement("h1",{},"System Rehega")
+]))
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(div);
+
